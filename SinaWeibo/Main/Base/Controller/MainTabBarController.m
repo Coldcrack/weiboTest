9c3d5acc79eb67cab4a0711c3d6e5aeac264ca6e
Coldcrack <13225542075@163.com>
2017-06-08 11:11:00 +0800

    2017-6-8

--- SinaWeibo/Main/Base/Controller/MainTabBarController.m
+++ SinaWeibo/Main/Base/Controller/MainTabBarController.m
@@ -9,7 +9,9 @@
 #import "MainTabBarController.h"
 
 @interface MainTabBarController ()
-
+{
+    UIImageView *_arrowImageView; //切换界面时，显示当前页面的标记
+}
 @end
 
 @implementation MainTabBarController
@@ -33,7 +35,7 @@
         }
         //将读取到的视图控制器交给标签控制器处理
         self.viewControllers = [mArray copy];
-        
+        [self customTabBar];
     }
     return self;
 }
@@ -50,15 +52,45 @@
             [tabBarButton removeFromSuperview];
         }
     }
+    //按钮宽度
+    CGFloat buttonWidth = [UIScreen mainScreen].bounds.size.width / 5;
 //    2.自定义创建标签栏按钮，并添加点击事件
+    for (int i=0; i<5; i++) {
+        CGRect buttonFrame = CGRectMake(i*buttonWidth, 0, buttonWidth, 49);
+        //创建按钮对象
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = buttonFrame;
+        [self.tabBar addSubview:button];
+        NSString *imageName = [NSString stringWithFormat:@"Skins/bluemoon/home_tab_icon_%i.png",i+1];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [button setImage:image forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(barButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+        button.tag = i;
+    }
     
-//    3.标签栏背景设定
-//    4.点击选中的视图创建
+    //3.标签栏背景设定
+    UIImage *image = [UIImage imageNamed:@"Skins/bluemoon/mask_navbar.png"];
+    [self.tabBar setBackgroundImage:image];
+    //标签栏阴影线
+    self.tabBar.shadowImage = [[UIImage alloc] init];
+    //4.点击选中的视图创建
+    //home_bottom_tab_arrow.png
+    _arrowImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, buttonWidth, 49)];
+//    [self.tabBar addSubview:_arrowImageView];
+    [self.tabBar insertSubview:_arrowImageView atIndex:0];
+    _arrowImageView.image = [UIImage imageNamed:@"Skins/bluemoon/home_bottom_tab_arrow.png"];
     
 }
-//视图即将渲染
--(void)viewWillAppear:(BOOL)animated {
-    [self customTabBar];
+-(void)barButtonAction:(UIButton *)button {
+//    NSLog(@"点击一次");
+    NSInteger tag = button.tag;
+    if (tag>=0 && tag<self.viewControllers.count) {
+        self.selectedIndex = tag;
+    }
+    //选中标记移动
+    [UIView animateWithDuration:0.2 animations:^{
+        _arrowImageView.center = button.center;
+    }];
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
